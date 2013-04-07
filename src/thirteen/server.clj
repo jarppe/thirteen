d8542adec67bb05e2a037fc1cf1083d20fff0d22
jarppe <jarppe@metosin.fi>
2013-04-07 21:14:25 +0300

    check that access comes from github

--- src/thirteen/server.clj
+++ src/thirteen/server.clj
@@ -8,22 +8,39 @@
             [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [cheshire.core :as c]
+            [slingshot.slingshot :refer [throw+]]
             [thirteen.util :as util]))
 
 (defn commit [payload]
-  (println "SUCCESS:")
-  (pprint payload)
-  (status 200 "ok"))
+  (let [pusher (:pusher payload)
+        user (:name pusher)
+        email (:email pusher)
+        branch (second (re-find #"^refs\/heads\/(.*)$" (:ref payload)))]
+    (println (format "PUSH: branch=%s, user=%s (%s)" branch user email))
+    (status 200 "ok")))
+
+(defn github [request]
+  (let [ip            (get-in request [:headers "x-real-ip"])
+        from-github?  (util/github-ip? ip)
+        payload       (get-in request [:params :payload])]
+    (when-not from-github?
+      (println "warn: attempted access from non-github address:" ip)
+      (throw+ {:status 403}))
+    (if-not payload
+      (println "warn: missing payload")
+      (throw+ {:status 400}))
+    (commit (c/parse-string payload true))))
 
 (def app-routes
-  [(POST "/github/commit" [payload] (when payload (commit (c/parse-string payload true))))
+  [(POST "/github/commit" [] github)
    (GET "/ping" [] (json "pong"))
    (route/not-found "not found dude")])
 
 (def prod-app
   (-> app-routes
     nm/app-handler
-    util/wrap-json))
+    util/wrap-json
+    util/wrap-sling))
 
 (def dev-app
   (-> prod-app
--- src/thirteen/util.clj
+++ src/thirteen/util.clj
@@ -1,9 +1,45 @@
 (ns thirteen.util
   (:require [clojure.walk :refer [keywordize-keys]]
             [clojure.java.io :as io]
-            [cheshire.core :as json]))
+            [noir.response :as resp]
+            [cheshire.core :as json]
+            [slingshot.slingshot :refer [throw+ try+]]))
 
-(defn starts-with [^String s ^String prefix]
+(def github-addresses ["207.97.227.253/32"
+                       "50.57.128.197/32"
+                       "108.171.174.178/32"
+                       "50.57.231.61/32"
+                       "204.232.175.64/27"
+                       "192.30.252.0/22"])
+
+(defn- ->int [s]
+  (Integer/parseInt s))
+
+(defn- ->ip
+  ([address]
+    (apply ->ip (map ->int (drop 1 (re-find #"(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})" address)))))
+  ([a1  a2 a3 a4]
+    (bit-or (bit-shift-left a1 24)
+            (bit-shift-left a2 16)
+            (bit-shift-left a3 8)
+            a4)))
+
+(defn- ->mask [mask-len]
+  (bit-and 0xFFFFFFFF (bit-shift-left 0xFFFFFFFF (- 32 mask-len))))
+
+(defn- ->cidr [address]
+  (let [[a1 a2 a3 a4 mask] (map ->int (drop 1 (re-find #"(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/(\d{1,2})" address)))]
+    [(->ip a1 a2 a3 a4) (bit-and 0xFFFFFFFF (bit-shift-left 0xFFFFFFFF (- 32 mask)))]))
+
+(defn- cidr-match [source [target mask]]
+  (= (bit-and source mask) target))
+
+(def ^:private github-cidrs (map ->cidr github-addresses))
+
+(defn github-ip? [address]
+  (some (partial cidr-match (->ip address)) github-cidrs))
+
+(defn starts-with [^String s prefix]
   (when (and s prefix)
     (.startsWith s prefix)))
 
@@ -19,4 +55,16 @@
           request (assoc request :json json-body)
           request (if json-body (assoc request :params json-body) request)]
       (handler request))))
+
+(defn wrap-sling [handler]
+  (fn [request]
+    (try+
+      (handler request)
+      (catch map? {:keys [status message]}
+        (resp/status status (or message ""))))))
+
+(try+
+  (throw+ {:status "foo"})
+  (catch map? {status :status message :message}
+    (println "Ups:" status)))
 
